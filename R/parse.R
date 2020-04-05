67be2720e682148331e965effcc018466c244bbb
Tuomo Nieminen <tuomonieminen@THL-MACTM-L214.local>
2020-04-05 21:23:56 +0300

    datasets retrieval and draft for cube object retrieval

--- R/parse.R
+++ R/parse.R
@@ -17,3 +17,15 @@ parse_subject <- function(x) {
 parse_hydra <- function(x) {
   parse_api_meta(x)
 }
+
+
+#' Parse dataset hrefs into a matrix with colums lang, subject, hydra, data matrix
+parse_datasets <- function(x, base_url) {
+  refs <- gsub(base_url, "", x$href)
+  m <- matrix(unlist(strsplit(refs, split = "/")), nrow = nrow(x), byrow = TRUE)
+  colnames(m) <- c("lang", "subject", "hydra", "cube")
+  m[, "cube"] <- gsub("^fact_|\\.json$", "",m[, "cube"])
+
+  df <- tibble::as_tibble(m)
+  cbind(tibble::tibble(base_url), df)
+}
--- R/retrieve.R
+++ R/retrieve.R
@@ -18,7 +18,9 @@ getHydra <- function(subject, hydra, api = url_api()) {
   url <- url_hydra(subject = subject, hydra = hydra, api = api)
   x <- getFromAPI(url)
   hydra <- parse_hydra(x)
-  structure(hydra, class = c(class(hydra), "thl_hydra"))
+  structure(hydra,
+            class = c(class(hydra), "api_response", "thl_hydra"),
+            api_data_url = attr(x, "api_data_url"))
 }
 
 #' @describeIn getHydra Get subject
@@ -28,10 +30,12 @@ getHydra <- function(subject, hydra, api = url_api()) {
 #'
 #' @export
 getSubject <- function(subject, api = url_api()) {
-  url <- url_subject(subject, api = api)
-  x <- getFromAPI(url)
+  data_url <- url_subject(subject, api = api)
+  x <- getFromAPI(data_url)
   subj <- parse_subject(x)
-  structure(subj, class = c(class(subj), "thl_subject"))
+  structure(subj,
+            class = c(class(subj), "api_response", "thl_subject"),
+            api_data_url = attr(x, "api_data_url"))
 }
 
 #' @describeIn getHydra Show data sets for subject or hydra
@@ -40,12 +44,54 @@ getSubject <- function(subject, api = url_api()) {
 #'
 #' @examples
 #'
-#' subject <- getSubject("toitu")
-#' showDatasets(subject)
+#' x <- getSubject("epirapo")
+#' datasets <- getDatasets(x)
 #'
 #' @export
-showDatasets <- function(x, api = url_api()) {
-  x[x$class == "dataset", ]
+getDatasets <- function(x) {
+
+  api_url <- api_url_from_response(x)
+  base_url <- api2base(api_url)
+  df <- x[x$class == "dataset", ]
+  df2 <- parse_datasets(df, base_url = base_url)
+  structure(df2,
+            class = c(class(df2), "api_datasets"))
+}
+
+
+#' Get cube
+#'
+#' @param datasets An object of class "api_datasets" object,
+#' or a data frame with columns 'lang', 'subject', 'hydra', 'cube'.
+#' @param index an integer defining the dataset of interest from the datasets data.frame.
+#'
+#' @examples
+#'
+#' x <- getSubject("epirapo")
+#' datasets <- getDatasets(x)
+#' cube <- getCube(datasets, index = 1L)
+#'
+getCube <- function(datasets, index = 1L) {
+
+  # The idea is that the object returned by this function
+  # would know it's location and dimensions
+  # then methods can be written to retrieve data.
+
+
+  if(index > nrow(datasets)) stop("Index is larger than the number of datasets")
+  ds <- datasets[index, , drop = FALSE]
+
+  fact_path <- paste0(ds$lang, "/", ds$subject, "/", ds$hydra, "/fact_",ds$cube)
+  dimension_path <-  paste0(fact_path, ".dimensions")
+  dimensionURL <- api_data_url(api = ds$base_url,
+               path = dimension_path,
+               format = "json")
+
+  dimensions <- getFromAPI(dimensionURL)
+
+  # TODO: the messy dimensions object needs a smart format and methods to handle it
+  list(dataset = ds,
+       dimensions = dimensions)
 }
 
 
