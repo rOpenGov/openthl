e422c93509be28be65cdd14d5e03f414f4cddcf3
Tuomo Nieminen <tuomonieminen@THL-MACTM-L214.local>
2020-11-02 12:01:49 +0200

    dev vignette to aid development

--- vignettes/main.R
+++ vignettes/main.R
@@ -3,4 +3,6 @@ library(rmarkdown)
 render("vignette.Rmd", output_format = "md_document")
 render("vignette.Rmd", output_format = "html_document")
 
+render("development.Rmd", output_format = "md_document")
+render("development.Rmd", output_format = "html_document")
 
